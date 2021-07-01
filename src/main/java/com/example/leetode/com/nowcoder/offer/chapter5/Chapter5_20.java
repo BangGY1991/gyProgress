1e772655ca1cc02fa542b89df04cda92820bc714
BangGY1991 <931476143@qq.com>
2021-07-01 16:11:16 +0800

    旋变字符串问题

---
+++ src/main/java/com/example/leetode/com/nowcoder/offer/chapter5/Chapter5_20.java
@@ -0,0 +1,120 @@
+package com.example.leetode.com.nowcoder.offer.chapter5;/**
+ * @program: gyProgress
+ * @description: 旋变字符串问题
+ * @author: GuoYu
+ * @create: 2021-06-23 14:12
+ **/
+
+/**
+ * @program: gyProgress
+
+ **/
+public class Chapter5_20 {
+
+    public boolean sameTypeSameNumber(char[] str1,char[] str2)
+    {
+        if (str1.length!=str2.length)
+        {
+            return false;
+        }
+        int[] map=new int[256];
+        for (int i = 0; i < str1.length; i++) {
+            map[str1[i]]++;
+        }
+        for (int i = 0; i < str2.length ; i++) {
+            if (--map[str2[i]]<0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean process(char[] str1,char[] str2,int L1,int L2,int size)
+    {
+        if (size==1)
+        {
+            return str1[L1]==str2[L2];
+        }
+        for (int leftPart = 1; leftPart <size; leftPart++) {
+            if (((process(str1,str2,L1,L2,leftPart)&&
+                    process(str1,str2,L1+leftPart,L2+leftPart,size-leftPart))||
+
+                    (process(str1,str2,L1,L2+size-leftPart,leftPart) &&
+                            process(str1,str2,L1=leftPart,L2,size-leftPart))))
+            {
+                return true;
+            }
+
+
+        }
+       return false;
+    }
+    public boolean isScramble1(String s1,String s2)
+    {
+        if ((s1==null&&s2!=null)||(s2==null&&s1!=null))
+        {
+            return false;
+        }
+        if (s1==null&&s2==null)
+        {
+            return true;
+        }
+        char[] str1=s1.toCharArray();
+        char[] str2=s2.toCharArray();
+        if (!sameTypeSameNumber(str1,str2))
+        {
+            return false;
+        }
+        int N=s1.length();
+
+      return process(str1,str2,0,0,N);
+    }
+
+    public boolean isScramble2(String s1,String s2) {
+        if ((s1 == null && s2 != null) || (s2 == null && s1 != null)) {
+            return false;
+        }
+        if (s1 == null && s2 == null) {
+            return true;
+        }
+        if (s1.equals(s2))
+        {
+            return true;
+        }
+        char[] str1=s1.toCharArray();
+        char[] str2=s2.toCharArray();
+        if (!sameTypeSameNumber(str1,str2))
+        {
+            return false;
+        }
+        int N=s1.length();
+        boolean[][][] dp=new boolean[N][N][N+1];
+        for (int L1 = 0; L1 <N ; L1++) {
+            for (int L2 = 0; L2 < N; L2++) {
+                dp[L1][L2][1] = str1[L1] == str2[L2];
+            }
+        }
+            for (int size = 2; size <=N ; size++) {
+                for (int L1 = 0; L1 <=N-size ; L1++) {
+                    for (int L2 = 0; L2 <=N-size ; L2++) {
+                        for (int leftPart = 0; leftPart <size; leftPart++) {
+                            if (
+                                    (dp[L1][L2][leftPart]&&dp[L1+leftPart][L2+leftPart][size-leftPart]) ||
+                                    (dp[L1][L2+size-leftPart][leftPart]&&dp[L1+leftPart][L2][size-leftPart])
+                            ){
+                                dp[L1][L2][size]=true;
+                                break;
+                            }
+
+
+                        }
+                    }
+                }
+
+            }
+
+        return dp[0][0][N];
+    }
+
+
+}
