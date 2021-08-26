36d33c098e70e4d2ed15c3d8b4404b410126ace4
BangGY1991 <931476143@qq.com>
2021-08-26 21:53:10 +0800

    数组中子数组的最大累乘积

---
+++ src/main/java/com/example/leetode/com/nowcoder/offer/chapter8/Chapter8_18.java
@@ -0,0 +1,30 @@
+package com.example.leetode.com.nowcoder.offer.chapter8;
+
+/**
+ * @program: gyProgress
+ * @description: 数组中子数组的最大累乘积
+ * @author: GuoYu
+ * @create: 2021-08-26 19:02
+ **/
+public class Chapter8_18 {
+    public double maxProduct(double[] arr)
+    {
+        if(arr==null||arr.length==0)
+        {
+            return 0;
+        }
+        double max=arr[0];
+        double min=arr[0];
+        double res=arr[0];
+        double maxEnd=0;
+        double minEnd=0;
+        for (int i = 0; i < arr.length; ++i) {
+            maxEnd=max*arr[i];
+            minEnd=min*arr[i];
+
+            res=Math.max(res,max);
+
+        }
+        return res;
+    }
+}
