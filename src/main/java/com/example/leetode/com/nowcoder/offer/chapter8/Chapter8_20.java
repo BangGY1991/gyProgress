9defc05dc2918ac040335f32038e9777f1384f0a
BangGY1991 <931476143@qq.com>
2021-08-30 12:57:11 +0800

    边界都是1的最大正方形大小

---
+++ src/main/java/com/example/leetode/com/nowcoder/offer/chapter8/Chapter8_20.java
@@ -0,0 +1,68 @@
+package com.example.leetode.com.nowcoder.offer.chapter8;
+
+/**
+ * @program: gyProgress
+ * @description: 边界都是1的最大正方形大小
+ * @author: GuoYu
+ * @create: 2021-08-27 18:33
+ **/
+public class Chapter8_20 {
+
+    public int getMaxSize(int[][] m) {
+        int[][] right = new int[m.length][m[0].length];
+        int[][] down = new int[m.length][m[0].length];
+        setBorderMap(m, right, down);
+        for (int size = Math.min(m.length, m[0].length); size != 0; size--) {
+            if (hasSizeOfBorder(size, right, down)) {
+                return size;
+            }
+        }
+
+
+        return 0;
+    }
+
+    private boolean hasSizeOfBorder(int size, int[][] right, int[][] down) {
+        for (int i = 0; i != right.length - size + 1; i++) {
+            for (int j = 0; j != right[0].length - size + 1; j++) {
+                if (right[i][j] >= size && down[i][j] >= size
+                        && right[i + size - 1][j] >= size
+                        && down[i][j + size - 1] >= size) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void setBorderMap(int[][] m, int[][] right, int[][] down) {
+        int r = m.length;
+        int c = m[0].length;
+        if (m[r - 1][c - 1] == 1) {
+            right[r - 1][c - 1] = 1;
+            down[r - 1][c - 1] = 1;
+        }
+        for (int i = r - 2; i != -1; i--) {
+            if (m[i][c - 1] == 1) {
+                right[i][c - 1] = 1;
+                down[i][c - 1] = down[i + 1][c - 1] + 1;
+            }
+        }
+        for (int i = r - 2; i != -1; i--) {
+            if (m[r - 1][i] == 1) {
+                right[r - 1][i] = right[r - 1][i + 1] + 1;
+                down[r - 1][i] = 1;
+            }
+        }
+        for (int i = r - 2; i != -1; i--) {
+            for (int j = c - 2; j != -1; j--) {
+                if (m[i][j] == 1) {
+                    right[i][j] = right[i][j + 1] + 1;
+                    down[i][j] = down[i + 1][j] + 1;
+                }
+            }
+        }
+    }
+
+
+}
