63e23adb966563b17eac7b5abab83f755d18030a
BangGY1991 <82643317+BangGY1991@users.noreply.github.com>
2021-12-11 16:30:08 +0800

    Manacher算法
    给定一个字符串str，返回str中最长回文子串的长度。

---
+++ src/main/java/com/example/leetode/com/nowcoder/offer/chapter9/Chapter9_32.java
@@ -0,0 +1,81 @@
+package com.example.leetode.com.nowcoder.offer.chapter9;
+
+/**
+ * @Author guoyu
+ * @Date 2021/12/11 15:58
+ * @Version 1.0
+ */
+public class Chapter9_32 {
+    public char[] manacherString(String str) {
+        char[] charArr = str.toCharArray();
+        char[] res = new char[str.length() * 2 + 1];
+        int index = 0;
+        for (int i = 0; i != res.length; i++) {
+            res[i] = (i & 1) == 0 ? '#' : charArr[index++];
+        }
+        return res;
+    }
+
+    public int maxLcpsLength(String str) {
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+        char[] charArr = manacherString(str);
+        int[] pArr = new int[charArr.length];
+        int index = -1;
+        int pR = -1;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i != charArr.length; i++) {
+            pArr[i] = pR > i ? Math.min(pArr[2 * index - 1], pR - i) : 1;
+            while (i + pArr[i] < charArr.length && i - pArr[i] > -1) {
+                if (charArr[i + pArr[i]] == charArr[i + pArr[i]]) {
+                    pArr[i]++;
+                } else {
+                    break;
+                }
+            }
+            if (i + pArr[i] > pR) {
+                pR = i + pArr[i];
+                index = i;
+            }
+            max = Math.max(max, pArr[i]);
+        }
+        return max - 1;
+    }
+
+
+    public String shortestEnd(String str) {
+        if (str == null || str.length() == 0) {
+            return null;
+        }
+        char[] charArr = manacherString(str);
+        int[] pArr = new int[charArr.length];
+        int index = -1;
+        int pR = -1;
+        int maxContainsEnd=-1;
+        for (int i = 0; i != charArr.length; i++) {
+            pArr[i] = pR > i ? Math.min(pArr[2 * index - 1], pR - i) : 1;
+            while (i + pArr[i] < charArr.length && i - pArr[i] > -1) {
+                if (charArr[i + pArr[i]] == charArr[i + pArr[i]]) {
+                    pArr[i]++;
+                } else {
+                    break;
+                }
+            }
+            if (i + pArr[i] > pR) {
+                pR = i + pArr[i];
+                index = i;
+
+            }
+            if (pR == charArr.length) {
+                maxContainsEnd = pArr[i];
+                break;
+            }
+        }
+            char[] res=new char[str.length()-maxContainsEnd+1];
+            for (int i = 0; i < res.length; i++) {
+               res[res.length-1-i]=charArr[i*2+1];
+            }
+        return String.valueOf(res);
+    }
+}
